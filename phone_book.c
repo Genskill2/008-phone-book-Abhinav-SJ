6e1634b772b1e29bf64092749e1234335a73e909
Abhinav-SJ <73390621+Abhinav-SJ@users.noreply.github.com>
2021-07-08 18:06:58 +0530

    Update phone_book.c

--- phone_book.c
+++ phone_book.c
@@ -73,7 +73,7 @@ int main(int argc, char *argv[]) {
         exit(1);
       }
       fclose(fp);
-      printf("%d",v);
+      printf("%ld",v);
       exit(0);
       
     //printf("NOT IMPLEMENTED!\n"); /* TBD  */
